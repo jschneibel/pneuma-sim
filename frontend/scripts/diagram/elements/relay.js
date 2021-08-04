4f5d6802ba6d98880d13696e609ee97433dd41fc
jschneibel <jonathan.schneibel@gmail.com>
2021-08-04 22:28:48 +0800

    add relay element

---
+++ frontend/scripts/diagram/elements/relay.js
@@ -0,0 +1,50 @@
+import createStandardElement from "./utils/standardElement.js";
+
+import mixinElectricCurrent from "./mixins/mixinElectricCurrent.js";
+
+export default function createRelay() {
+  const type = "relay";
+  const width = 40;
+  const height = width;
+
+  const relay = createStandardElement({
+    type,
+    dimensions: { width, height },
+    terminalDefinitions: [
+      { x: width / 2, y: 0, medium: "electric" },
+      { x: width / 2, y: height, medium: "electric" },
+    ],
+    draw,
+  });
+
+  mixinElectricCurrent({
+    element: relay,
+    resistance: 1,
+  });
+
+  // in element-local coordinates
+  function draw(ctx) {
+    const { width, height } = relay.getDimensions();
+    const boxHeight = (2 / 5) * width;
+    const boxOffset = (height - boxHeight) / 2;
+
+    ctx.beginPath();
+
+    // box
+    ctx.moveTo(0, boxOffset);
+    ctx.lineTo(width, boxOffset);
+    ctx.lineTo(width, boxOffset + boxHeight);
+    ctx.lineTo(0, boxOffset + boxHeight);
+    ctx.lineTo(0, boxOffset);
+
+    // terminal lines
+    ctx.moveTo(width / 2, 0);
+    ctx.lineTo(width / 2, boxOffset);
+    ctx.moveTo(width / 2, boxOffset + boxHeight);
+    ctx.lineTo(width / 2, height);
+
+    ctx.stroke();
+  }
+
+  return relay;
+}
