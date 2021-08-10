6a06d65681644e1b57f065dc45574e5c38539085
jschneibel <jonathan.schneibel@gmail.com>
2021-08-10 19:39:03 +0800

    fix mousemove error on window edge

--- frontend/scripts/input/editing/handleMouseMove.js
+++ frontend/scripts/input/editing/handleMouseMove.js
@@ -2,6 +2,12 @@ import { getTransformedMousePosition } from "../utils/mousePosition.js";
 
 export default function handleMouseMove(event, canvas, ctx, diagram) {
   const mousePosition = getTransformedMousePosition(event, canvas, ctx);
+  if (!mousePosition) {
+    // When the mouse is at the edge of the window, no coordinates
+    // are given in the mousemove event. Breaking here avoids
+    // errors further downstream.
+    return;
+  }
 
   for (const element of diagram.getElements()) {
     for (const terminal of element.getTerminals?.() || []) {
