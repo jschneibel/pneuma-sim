ac86d0b1e5efa965f4d642cf70263289b00bfdd4
jschneibel <37653547+jschneibel@users.noreply.github.com>
2021-08-15 15:40:19 +0200

    comment and fix propagating arguments

--- frontend/scripts/diagram/elements/mixins/mixinActive.js
+++ frontend/scripts/diagram/elements/mixins/mixinActive.js
@@ -1,3 +1,23 @@
+/**
+ * @file Mixin for elements to have an 'active' state.
+ * @author Jonathan Schneibel
+ */
+
+/**
+ * Mixes a boolean 'active' state into the given element.
+ * The state can be changed with element.activate(),
+ * element.deactivate() and element.toggleActivate().
+ * element.activate(args) and element.deactivate(args) will cause
+ * the callbacks onActivate(args) and onDeactivate(args) and return their results.
+ * The callbacks are only called if the 'active' state effectively changes
+ * (e.g. calling element.activate() twice will call onDeactivate() only once).
+ *
+ * @param {object} param0 Options object.
+ * @param {object} param0.element Element to mix 'active' state into.
+ * @param {boolean} param0.active Initial value.
+ * @param {Function} param0.onActivate Callback to call whenever the element is activated.
+ * @param {Function} param0.onDeactivate Callback to call whenever the element is deactivated.
+ */
 export default function mixinActive({
   element = {},
   active = false,
@@ -9,7 +29,7 @@ export default function mixinActive({
   element.activate = function () {
     if (!active) {
       active = true;
-      return onActivate(arguments);
+      return onActivate(...arguments);
     } else {
       return undefined;
     }
@@ -18,7 +38,7 @@ export default function mixinActive({
   element.deactivate = function () {
     if (active) {
       active = false;
-      return onDeactivate(arguments);
+      return onDeactivate(...arguments);
     } else {
       return undefined;
     }
