034e18bd56a28bf1c5c50e01010a259200642a26
jschneibel <37653547+jschneibel@users.noreply.github.com>
2021-08-15 17:06:46 +0200

    add jsdoc

--- frontend/scripts/diagram/elements/mixins/mixinHighlighting.js
+++ frontend/scripts/diagram/elements/mixins/mixinHighlighting.js
@@ -1,3 +1,20 @@
+/**
+ * @file Mixin for elements to have a 'highlighted' state.
+ * @author Jonathan Schneibel
+ * @module
+ */
+
+/**
+ * Mixes a boolean 'highlighted' state into the given element.
+ * The state can be read with element.isHighlighted(), changed
+ * with element.highlight() and element.unhighlight().
+ * Side effects such as graphical representation of the highlighted
+ * state are not added with this mixin.
+ *
+ * @param {object} param0 Options object.
+ * @param {object} param0.element Element to mix 'highlighted' state into.
+ * @param {boolean} param0.highlighted Initial value.
+ */
 export function mixinHighlighting({ element, highlighted = false }) {
   element.isHighlighted = () => highlighted;
 
