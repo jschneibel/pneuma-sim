68bfe70521ead16a592174cf9db93d3579be5e39
jschneibel <37653547+jschneibel@users.noreply.github.com>
2021-08-15 17:54:18 +0200

    add jsdoc

--- frontend/scripts/diagram/elements/mixins/mixinPosition.js
+++ frontend/scripts/diagram/elements/mixins/mixinPosition.js
@@ -1,3 +1,21 @@
+/**
+ * @file Mixin for elements to have a position.
+ * @author Jonathan Schneibel
+ * @module
+ */
+
+/**
+ * Mixes a {{ x: number, y: number }} position into the given element.
+ * The position can be read with element.getPosition() and changed
+ * with element.setPosition(). Changing the intial position object directly
+ * won't have an effect on the position of the element.
+ * Side effects such as graphical representation of the position
+ * are not added with this mixin.
+ *
+ * @param {object} param0 Options object.
+ * @param {object} param0.element Element to mix 'highlighted' state into.
+ * @param {{ x: number, y: number }} param0.position Initial value.
+ */
 export function mixinPosition({ element, position = { x: 0, y: 0 } }) {
   let x = position.x;
   let y = position.y;
--- frontend/scripts/diagram/elements/mixins/mixinRemoval.js
+++ frontend/scripts/diagram/elements/mixins/mixinRemoval.js
@@ -1,3 +1,19 @@
+/**
+ * @file Mixin for elements to perform an action when removed from the diagram.
+ * @author Jonathan Schneibel
+ * @module
+ */
+
+/**
+ * Mixes a callback into the element that is called whenever element.remove()
+ * is called (this happens e.g. when an element is deleted from the diagram).
+ * If the mixin is applied to the same element multiple times, all the supplied
+ * the callbacks are executed in the order the respective mixins have been called.
+ *
+ * @param {object} param0 Options object.
+ * @param {object} param0.element Element to mix the callback into.
+ * @param {Function} param0.remove Callback to be called upon element removal.
+ */
 export function mixinRemoval({ element, remove = function (diagram) {} }) {
   // extend remove function if it already has been mixed-in
   const existingRemove = element.remove;
